11e283f471611a4868c182e8bf2a26f799a3e55a
vainila <workmail1421@gmail.com>
2022-01-25 11:03:05 +0300

    upd

--- react-course/ra2/src/App.js
+++ react-course/ra2/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+   state = {
+      number: 47,
+   };
+
+   handleClick = () => {
+      this.setState(
+         (prevState, prevProps) => ({
+            number: prevState.number + 1,
+         }),
+         () => console.log("updated state" + this.state.number)
+      );
+   };
+   render() {
+      return (
+         <div className="App">
+            <header className="App-header">
+               <img src={logo} className="App-logo" alt="logo" />
+               <p>{this.state.number}</p>
+               <button onClick={this.handleClick} className="App-link">
+                  Update state
+               </button>
+            </header>
+         </div>
+      );
+   }
 }
 
 export default App;
